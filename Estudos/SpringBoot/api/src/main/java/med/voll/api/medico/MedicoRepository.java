214a089e11b49cd7d1d5a045c765138be87a71d3
PHVital <vital.ph.g@gmail.com>
2025-12-22 12:00:49 -0300

    feat: Requisições GET

--- Estudos/SpringBoot/api/src/main/java/med/voll/api/medico/MedicoRepository.java
+++ Estudos/SpringBoot/api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -2,5 +2,7 @@ package med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
