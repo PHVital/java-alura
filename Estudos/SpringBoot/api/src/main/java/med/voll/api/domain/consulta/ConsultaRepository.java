652f8c1dd15b07a5e6da8814c0fb5cd3e7eb0a68
PHVital <vital.ph.g@gmail.com>
2026-01-13 13:07:54 -0300

    feat: Adiciona SpringDoc para documentação da API e Swagger UI para vizualizar e testar a API Rest

--- Estudos/SpringBoot/api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ Estudos/SpringBoot/api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-    boolean existsByMedicoIdandData(Long idMedico, LocalDateTime data);
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 
     boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
