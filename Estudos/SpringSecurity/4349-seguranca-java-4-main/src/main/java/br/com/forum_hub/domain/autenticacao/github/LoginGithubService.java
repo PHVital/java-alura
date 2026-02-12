cdae44ea88db830fd39be7ede711339ea079d097
PHVital <vital.ph.g@gmail.com>
2026-02-12 14:09:23 -0300

    feat: extraindo informações com scopes via OAuth

--- Estudos/SpringSecurity/4349-seguranca-java-4-main/src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGithubService.java
+++ Estudos/SpringSecurity/4349-seguranca-java-4-main/src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGithubService.java
@@ -1,5 +1,6 @@
 package br.com.forum_hub.domain.autenticacao.github;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -22,10 +23,10 @@ public class LoginGithubService {
         return "https://github.com/login/oauth/authorize" +
                 "?client_id=" + clientId +
                 "&redirect_uri=" + redirectUri +
-                "&scope=read:user,user:email";
+                "&scope=user:email";
     }
 
-    public String gerarToken(String code) {
+    private String obterToken(String code) {
         var resposta = restClient.post()
                 .uri("https://github.com/login/oauth/access_token")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -33,7 +34,31 @@ public class LoginGithubService {
                 .body(Map.of("code", code, "client_id", clientId,
                         "client_secret", clientSecret, "redirect_uri", redirectUri))
                 .retrieve()
-                .body(String.class);
-        return resposta;
+                .body(Map.class);
+        System.out.println(resposta);
+
+        return resposta.get("access_token").toString();
+    }
+
+    public String obterEmail(String code) {
+        var token = obterToken(code);
+
+        var headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        var resposta = restClient.get()
+                .uri("https://api.github.com/user/emails")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(DadosEmail[].class);
+
+        for(DadosEmail d : resposta) {
+            if(d.primary() && d.verified()) {
+                return d.email();
+            }
+        }
+
+        return null;
     }
 }
--- Estudos/SpringSecurity/4349-seguranca-java-4-main/src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
+++ Estudos/SpringSecurity/4349-seguranca-java-4-main/src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -10,4 +10,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByToken(String codigo);
 
     Optional<Usuario> findByNomeUsuarioIgnoreCaseAndVerificadoTrueAndAtivoTrue(String nomeUsuario);
+
+    Optional<Usuario> findByEmailIgnoreCaseAndVerificadoTrueAndAtivoTrue(String email);
 }
